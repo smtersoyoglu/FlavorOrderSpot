6a8bf5de33dd169ba44ea015b48bcb8f09fe3d5c
smter <smtersoyoglu231@gmail.com>
2023-11-12 21:03:28 +0300

    menu

--- app/src/main/java/com/sametersoyoglu/flavororderspot/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/sametersoyoglu/flavororderspot/ui/fragment/HomeFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.sametersoyoglu.flavororderspot.R
+import com.sametersoyoglu.flavororderspot.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,11 +23,15 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_home,container,false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.homeFragment = this
+        binding.toolbarTitle = "Hoşgeldiniz"
+
     }
 }

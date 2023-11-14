d512fdcd3f047c87dfed884706bf9fb82ffd6037
smter <smtersoyoglu231@gmail.com>
2023-11-14 23:15:06 +0300

    data

--- app/src/main/java/com/sametersoyoglu/flavororderspot/ui/adapter/FoodsAdapter.kt
+++ app/src/main/java/com/sametersoyoglu/flavororderspot/ui/adapter/FoodsAdapter.kt
@@ -1,4 +1,25 @@
 package com.sametersoyoglu.flavororderspot.ui.adapter
 
-class FoodsAdapter {
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sametersoyoglu.flavororderspot.data.entity.Foods
+import com.sametersoyoglu.flavororderspot.databinding.ItemFoodsBinding
+
+class FoodsAdapter (var mContext: Context, var foodsList: List<Foods>) : RecyclerView.Adapter<FoodsAdapter.FoodsViewHolder>(){
+
+    inner class FoodsViewHolder(var binding: ItemFoodsBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodsViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: FoodsViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
 }

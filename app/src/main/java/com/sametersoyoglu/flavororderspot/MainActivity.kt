0dedee2cb5719b1144883f370f52c6ff0a286fdd
smter <smtersoyoglu231@gmail.com>
2023-11-27 22:37:45 +0300

    cart

--- app/src/main/java/com/sametersoyoglu/flavororderspot/MainActivity.kt
+++ app/src/main/java/com/sametersoyoglu/flavororderspot/MainActivity.kt
@@ -2,12 +2,27 @@ package com.sametersoyoglu.flavororderspot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.sametersoyoglu.flavororderspot.databinding.ActivityMainBinding
+import com.sametersoyoglu.flavororderspot.ui.fragment.CartFragment
+import com.sametersoyoglu.flavororderspot.ui.fragment.FavoriteFragment
+import com.sametersoyoglu.flavororderspot.ui.fragment.HomeFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        NavigationUI.setupWithNavController(binding.bottomnavigation , navHostFragment.navController)
+
     }
 }

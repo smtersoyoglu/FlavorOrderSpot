0607bf03aecfd2c1efa01451f90dbb58c89b6ad8
smter <smtersoyoglu231@gmail.com>
2023-12-01 20:39:08 +0300

    cart

--- app/src/main/java/com/sametersoyoglu/flavororderspot/ui/fragment/CartFragment.kt
+++ app/src/main/java/com/sametersoyoglu/flavororderspot/ui/fragment/CartFragment.kt
@@ -5,15 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.sametersoyoglu.flavororderspot.R
+import com.sametersoyoglu.flavororderspot.databinding.FragmentCartBinding
+import com.sametersoyoglu.flavororderspot.ui.viewmodel.CartViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 
 class CartFragment : Fragment() {
+    private lateinit var binding: FragmentCartBinding
+    private lateinit var viewModel: CartViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // ViewModeli bağlama - onCreate içersinde olur bu işlem
+        val tempViewModel : CartViewModel by viewModels() // gecici bir viewmodele atayıp ordan bizim viewmodelimize bağlarız.
+        viewModel = tempViewModel
 
     }
 
@@ -25,5 +33,7 @@ class CartFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_cart, container, false)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }

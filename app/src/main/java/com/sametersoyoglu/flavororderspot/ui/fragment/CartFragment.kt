c7749ea4aa02d9936c091d9c0ad4ba38713e27c8
smter <smtersoyoglu231@gmail.com>
2023-12-01 20:44:03 +0300

    cart

--- app/src/main/java/com/sametersoyoglu/flavororderspot/ui/fragment/CartFragment.kt
+++ app/src/main/java/com/sametersoyoglu/flavororderspot/ui/fragment/CartFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import com.sametersoyoglu.flavororderspot.R
 import com.sametersoyoglu.flavororderspot.databinding.FragmentCartBinding
@@ -29,8 +30,8 @@ class CartFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_cart,container,false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
